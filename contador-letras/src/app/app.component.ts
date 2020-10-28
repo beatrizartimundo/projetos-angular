188e2b6da0ef9d7fc88a89a3d42aa31f30deed66
Beatriz Artimundo <beatriz.artimundo@gmail.com>
2020-10-28 15:10:28 -0300

    adicao do conteudo

--- contador-letras/src/app/app.component.ts
+++ contador-letras/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'contador-letras';
+export class AppComponent implements OnInit{
+  title = 'Contador';
+  desc: "";
+
+  constructor(){  }
+
+  ngOnInit(){}
+
+     contador(){
+       
+      var inputValue = (<HTMLInputElement>document.getElementById("text")).value;
+      //const abc = String.prototype.trim();     
+       document.getElementById("resultado").innerHTML =
+        "Sua frase tem"+  inputValue.length + "letras";
+      }
+      contadorTextarea(){
+        //utilizando o ngModel após declarar acima é só utilizar o this. nome da propriedade para pegar a quantidade
+             
+        document.getElementById("resultado2").innerHTML = 
+         "Sua area de texto tem  " + this.desc.length + "letras";
+          
+      };
+      	           
+  
 }
